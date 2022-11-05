3e07fc3523623dbd5e154312db89b4b93c857470
55555 <glinskaya33@mail.ru>
2022-11-05 23:06:15 +0300

    HWnew13

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,52 @@
+import java.util.Objects;
+
 public class Book {
+    private String titleOfTheBook;
+    private int yearOfPublicationOfTheBook;
+    private Author author;
+    Objects o;
+    public Book(String titleOfTheBook, int yearOfPublicationOfTheBook, Author fullName) {
+        this.titleOfTheBook = titleOfTheBook;
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+        this.author = new Author(author);
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public String titleOfTheBook() {
+        return this.titleOfTheBook;
+    }
+
+    public int getYearOfPublicationOfTheBook() {
+        return this.yearOfPublicationOfTheBook;
+    }
+
+    public void setYearOfPublicationOfTheBook(int yearOfPublicationOfTheBook) {
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublicationOfTheBook == book.yearOfPublicationOfTheBook && titleOfTheBook.equals(book.titleOfTheBook)
+                && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {return Objects.hash(titleOfTheBook, yearOfPublicationOfTheBook, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "titleOfTheBook='" + titleOfTheBook + '\'' +
+                ", yearOfThePublicationOfTheBook=" + yearOfPublicationOfTheBook +
+                ", Author=" + author +
+                '}';
+    }
+
 }
